742d4fa38d7e1a1b55e0fe7e35ed7353d823258e
Maximilian Grau <Maximilian.Grau@st.ovgu.de>
2018-02-15 18:44:03 +0100

    revert utf-8

--- TT-PlayerUnit/main.cpp
+++ TT-PlayerUnit/main.cpp
@@ -1,1 +1,15 @@
-﻿#include "main.h"  int main(void) {   timer0_init();   uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(UART_BAUD, F_CPU));   // Timer 0 konfiguieren   // UART konfiguieren    // Display initialisieren    while (1)   {   } } 
+#include "main.h"
+
+int main(void)
+{
+  timer0_init();
+  uart_init(UART_BAUD_SELECT_DOUBLE_SPEED(UART_BAUD, F_CPU));
+  // Timer 0 konfiguieren
+  // UART konfiguieren
+
+  // Display initialisieren
+
+  while (1)
+  {
+  }
+}
