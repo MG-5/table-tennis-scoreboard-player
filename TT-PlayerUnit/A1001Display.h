013fdbc6f07d8dbaaf8b2fd0d8ba103616e9ecf4
Maximilian Grau <Maximilian.Grau@st.ovgu.de>
2018-02-15 19:10:28 +0100

    second adapt A1001 Display

--- TT-PlayerUnit/A1001Display.h
+++ TT-PlayerUnit/A1001Display.h
@@ -17,11 +17,23 @@ public:
   void setBrightness(uint8_t brightness);
 
   //! Refresh display
+  void update(uint8_t currentDigit);
+
   void tick(uint32_t now);
 
-  void sendByte(uint8_t shiftByte);
+  void showScoreline();
 
-  void showScoreline_playerOne(uint8_t scoreFirstPlayer = 0, uint8_t scoreSecondPlayer = 0);
+  uint8_t *scoreline_format(uint8_t firstPlayer, uint8_t secondPlayer);
+
+  void showServes(uint8_t serves = 0);
+
+  void runningText(const uint8_t text[], uint16_t speed);
+
+  void startupSequence();
+
+  bool textIsRunning = false;
+
+  void setSegments(const bool doublepoint, const uint8_t digits[4]);
 
   //! Display arbitrary data on the module
 
@@ -30,24 +42,14 @@ public:
   //! @param digit2 - third digit
   //! @param digit3 - fourth digit
   //! @param doublepoint - show doublepoint or not
-
-  void showServes(uint8_t serves = 0);
-
-  void setSegments_playerOne(const bool doublepoint, const uint8_t digit1, const uint8_t digit2, const uint8_t digit3,
-                             const uint8_t digit4);
-
-  void runningText(const uint8_t text[], uint16_t speed);
-
-  void startupSequence();
-
-  bool textIsRunning = false;
+  void setSegments(const bool doublepoint, const uint8_t firstDigit, const uint8_t secondDigit,
+                   const uint8_t thirdDigit, const uint8_t forthDigit);
 
 private:
-  uint8_t _score_playerOne;
-  uint8_t _score_playerTwo;
+  uint8_t _score_myself;
+  uint8_t _score_other;
   uint8_t _brightness;
-  uint8_t _digit_playerOne[5];
-  uint8_t _digit_playerTwo[5];
+  uint8_t _digit[5];
   uint8_t letter[4];
   uint8_t textLength;
   uint8_t counter1;
@@ -57,6 +59,8 @@ private:
   uint8_t _text[5];
 
   void _runningText();
+
+  void _sendByte(uint8_t shiftByte);
 };
 
 #endif // __A1001DISPLAY__
